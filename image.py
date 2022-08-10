7afd041c217d6238179078e6b1524fa0cd7411b6
sayan <Sayantan.officialpurp@gamil.com>
2022-08-10 19:33:03 +0530

    code

--- image.py
+++ image.py
@@ -1,8 +1,20 @@
+import imp
 import cv2
 import easygui
 import numpy as np
 import imageio 
+import sys
+import matplotlib.pyplot as plt
+import os
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
 
-
-
+def upload():
+    ImagePath= easygui.fileopenbox()
+    cartoonify(ImagePath)
+def cartoonify(ImagePath):
+    originalmage= cv2.imread(ImagePath)
+    
 
